d4f637aad3a2b5a3eb1d95bd3e12388225efde03
Julian <julianrinaudo18@gmail.com>
2023-04-21 18:25:42 -0300

    fix

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Carrousel from "./Carrousel";
-import ImgJs from "../../assets/pseudo.png"
-import IconStar from "../../assets/63934072b143767c6aa620f8_start-icon.svg.png"
-import ImgCarrousel from "../..//assets/carrousel.png"
+import ImgJs from "../../assets/pseudo.png";
+import IconStar from "../../assets/63934072b143767c6aa620f8_start-icon.svg.png";
+import ImgCarrousel from "../..//assets/carrousel.png";
 const Home = () => {
   return (
-    <section className="flex flex-col bg-primary min-h-[100vh] relative">
+    <section className="flex flex-col bg-primary min-h-[100vh]">
       {/* <img
         className="md:w-24 lg:w-32 xl:w-32 2xl:w-32 translate-y-[-15px] hidden md:flex "
         src="src/assets/63a0c6b60bea3337228ddf1b_orange-bracket-icon.webp.png"
@@ -31,20 +31,16 @@ const Home = () => {
         alt="start icon"
       />
       <motion.div
-        initial={{ opacity: 0, scale:1.5 }}
+        initial={{ opacity: 0, scale: 1.5 }}
         transition={{ duration: 0.5 }}
-        whileInView={{ opacity: 1, scale:1}}
+        whileInView={{ opacity: 1, scale: 1 }}
         viewport={{ once: true }}
         className="home text-center text-3xl pt-52 pb-28 px-4"
       >
         <p className="home_name font-body font-semibold">Soy Julian Rinaudo</p>
         <div className="flex justify-center items-center text-4xl gap-2">
           <p className="home_stack font-body font-bold">FullStack</p>
-          <img
-            className=" object-contain w-24"
-            src={ImgJs}
-            alt="icon JS"
-          />
+          <img className=" object-contain w-24" src={ImgJs} alt="icon JS" />
         </div>
         <p className="home_dev text-4xl font-body font-bold">Developer</p>
       </motion.div>
@@ -58,9 +54,9 @@ const Home = () => {
           </button>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center absolute bottom-0">
+      <div className="flex flex-col justify-center items-center">
         <img
-          className="w-full max-[640px]:h-[240px] "
+          className="w-full max-[640px]:h-[240px]"
           src={ImgCarrousel}
           alt=""
         />
