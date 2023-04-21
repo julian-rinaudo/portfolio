41b235d9253252914963b1dead9c965197fd1d22
Julian <julianrinaudo18@gmail.com>
2023-04-21 20:28:35 -0300

    seccion proyectos creada

--- src/components/Proyects/index.jsx
+++ src/components/Proyects/index.jsx
@@ -1,4 +1,25 @@
 import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
+
 const Proyects = () => {
   return (
     <section className="proyects min-h-[200vh] w-[100%] bg-top bg-auto bg-emerald-500 ">
@@ -7,13 +28,54 @@ const Proyects = () => {
         transition={{ duration: 0.5 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.7 }}
-        className=" text-white text-center pt-10 text-[40px] font-body font-medium"
+        className=" text-white text-center py-20 text-[40px] font-body font-medium"
       >
         Proyectos
       </motion.h2>
-      <div>Proyecto1</div>
-      <div>Proyecto2</div>
-      <div>Proyecto3</div>
+      <div className="px-[20px] flex flex-col gap-[45px]">
+        <motion.div
+          initial={"hidden"}
+          variants={container}
+          transition={{ duration: 0.5 }}
+          whileInView={"visible"}
+          viewport={{ once: true, amount: 0.7 }}
+          className=" min-h-[336px] max-h-[700px]  max-w-[500px] s"
+        >
+          <motion.div
+          variants={item}
+          >
+            <div className="min-h-[220px] bg-primary"></div>
+          </motion.div>
+        </motion.div>
+        <motion.div
+          initial={"hidden"}
+          variants={container}
+          transition={{ duration: 0.5 }}
+          whileInView={"visible"}
+          viewport={{ once: true, amount: 0.7 }}
+          className=" min-h-[336px] max-h-[700px]  max-w-[500px] s"
+        >
+          <motion.div
+          variants={item}
+          >
+            <div className="min-h-[220px] bg-primary"></div>
+          </motion.div>
+        </motion.div>
+        <motion.div
+          initial={"hidden"}
+          variants={container}
+          transition={{ duration: 0.5 }}
+          whileInView={"visible"}
+          viewport={{ once: true, amount: 0.7 }}
+          className=" min-h-[336px] max-h-[700px]  max-w-[500px] s"
+        >
+          <motion.div
+          variants={item}
+          >
+            <div className="min-h-[220px] bg-primary"></div>
+          </motion.div>
+        </motion.div>
+      </div>
     </section>
   );
 };
