18a46212e1fb17268d0ae08b95ee286c5dcb74e2
Julian <julianrinaudo18@gmail.com>
2023-04-27 16:25:08 -0300

    arregle hover en vista mobile

--- src/commons/Btn.jsx
+++ src/commons/Btn.jsx
@@ -3,10 +3,10 @@ import loadingv2 from "react-useanimations/lib/loading2";
 
 const Btn = ({ text, loading }) => {
   return (
-    <div className="rounded-full drop-shadow-custom hover:drop-shadow-hover">
+    <div className="rounded-full drop-shadow-custom lg:hover:drop-shadow-hover">
       <button
-        className={`capitalize button font-body border-[2px] border-secondary rounded-full text-[17px] font-medium text-white py-[16px] px-[27px] bg-secondary hover:border-[2px] 
-        hover:border-secondary hover:bg-primary hover:text-secondary hover:rounded-[1rem]`}
+        className={`capitalize button font-body border-[2px] border-secondary rounded-full text-[17px] font-medium text-white py-[16px] px-[27px] bg-secondary lg:hover:border-[2px] 
+        lg:hover:border-secondary lg:hover:bg-primary lg:hover:text-secondary lg:hover:rounded-[1rem]`}
       >
         {loading ? <UseAnimations animation={loadingv2} /> : text}
       </button>
