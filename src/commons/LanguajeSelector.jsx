f7674a2ece0fe4b91e100e03f5795a780397b182
Julian <julianrinaudo18@gmail.com>
2023-05-14 23:19:25 -0300

    agregue idioma del browser del usuario

--- src/commons/LanguajeSelector.jsx
+++ src/commons/LanguajeSelector.jsx
@@ -1,20 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import i18n from "../config/i18n";
 
 function LanguageSelector() {
   const languaje = window.localStorage.i18nextLng;
+  const browserLanguaje = navigator.language.split("-")[0];
   const changeLanguage = (e) => {
-    window.location.reload()
+    window.location.reload();
     i18n.changeLanguage(e.target.value);
   };
 
   return (
-    <select value={languaje} onChange={changeLanguage} className="appearance-none outline-none text-[17px] px-4 rounded-md bg-transparent text-gray-700 cursor-pointer">
+    <select
+      value={languaje || browserLanguaje}
+      onChange={changeLanguage}
+      className="appearance-none outline-none text-[17px] px-4 rounded-md bg-transparent text-gray-700 cursor-pointer"
+    >
       <option value="es">🇪🇸 Español</option>
       <option value="en">🇬🇧 English</option>
     </select>
   );
 }
-
 
 export default LanguageSelector;
